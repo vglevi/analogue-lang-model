db3e8dd5552d8591bbfce640708fed78f97e5b55
vglevi <zsovalevente@gmail.com>
2026-05-01 20:40:41 +0200

    added assign_train_test

--- src/corpus.py
+++ src/corpus.py
@@ -1,3 +1,5 @@
+import random
+
 def process_txt(file: str) -> list[list[str]]:
     '''
     From a txt file, 
@@ -13,3 +15,21 @@ def process_txt(file: str) -> list[list[str]]:
             sentences.append(sentence.strip().split())
     
     return sentences
+
+def assign_train_test(corpus: list[list[str]], rate: float = 0.9) -> tuple[list[list[str]], list[list[str]]]:
+    '''
+    Shuffles the corpus then splits into a training and a test data based on the given rate.
+    The corpus argument should be a list of sentences (a sentence is a list of words).
+    The rate argument should be a float between 0 and 1, 
+    and it determines the size of the training data.
+    The base value of rate is 0.9.
+    Returns a tupple containing the training and the test data.
+    '''
+
+    if not 0 < rate < 1:
+        raise ValueError("Rate should be a float between 0 and 1")
+
+    randomized = random.sample(corpus, len(corpus))
+    split_index = round(len(corpus) * rate)
+
+    return randomized[:split_index], randomized[split_index:]

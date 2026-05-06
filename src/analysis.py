0e2696325c9c01d9a21699e5c668ea66979fb4ec
vglevi <zsovalevente@gmail.com>
2026-05-06 20:17:02 +0200

    Word: added description

--- src/analysis.py
+++ src/analysis.py
@@ -2,6 +2,12 @@ from collections import defaultdict
 from src.corpus import Sentences
 
 class Word:
+    '''
+    Attributes:
+    - freq: frequency of the word
+    - before: a dictionary containing which and how frequent words appear before the word
+    - after: a dictionary containing which and how frequent words appear after the word
+    '''
     def __init__(self) -> None:
         self.freq = 0
         self.before: defaultdict[str, int] = defaultdict(int)

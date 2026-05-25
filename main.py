c5525d81a89c5cde64144e0926ea6ec061cc611e
vglevi <zsovalevente@gmail.com>
2026-05-25 18:05:09 +0200

    optimizing main

--- main.py
+++ main.py
@@ -7,24 +7,21 @@ def main():
     corp = process_txt("norvig_corpus.txt")
     train, test = assign_train_test(corp, 0.999)
     word_dict = analyze_corpus(train)
-
-    result = defaultdict(dict)
-    
-    all_bigrams = set()
-
-    for sen in test:
-        all_bigrams.update(zip(sen, sen[1:]))
+    all_bigrams = {bg for sen in test for bg in zip(sen, sen[1:])}
 
     cache = {bg: find_analogies(word_dict, bg) for bg in all_bigrams}
+    result = defaultdict(dict)
 
     for bigram, anals in cache.items():
         for anal, p in anals.items():
             result[anal][bigram] = p
 
     with open("out.txt", "w") as f:
+        lines = []
         for anal, pb in result.items():
-            f.write(f"{anal}:\n")
-            f.writelines(f"\t{bigram}: {p}\n" for bigram, p in pb.items())
+            lines.append(f"{anal}:\n")
+            lines.extend(f"\t{bigram}: {p}\n" for bigram, p in pb.items())
+        f.writelines(lines)
 
 
 if __name__ == "__main__":

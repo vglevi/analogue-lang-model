03c055ed21f6682034482fd759e6f51010f627e2
vglevi <zsovalevente@gmail.com>
2026-05-25 17:55:03 +0200

    more little optim to find_analogies

--- src/analogy.py
+++ src/analogy.py
@@ -4,22 +4,27 @@ type Analogies = dict[tuple[str, str], float]
 
 def find_analogies(word_dict: WordDict, bigram: tuple[str, str]):
     analogies: Analogies = {}
-    w1_bigram = word_dict[bigram[0]]
+    get_word = word_dict.__getitem__ # avoiding global lookups per call
+
+    b1, b2 = bigram
+    w1_bigram = get_word(b1)
     w1_afters = w1_bigram.after
+    w1_freq = w1_bigram.freq
+
 
     for anal2, freq2 in w1_afters.items():
-        w2_anal = word_dict[anal2]
+        p_anal2_given_w1 = freq2 / w1_freq
+        w2_anal = get_word(anal2)
         befores_w2_anal = w2_anal.before
+        anal2_freq = w2_anal.freq
 
-        for anal1, freq1 in befores_w2_anal.items():
-            w1_anal = word_dict[anal1]
-            freq3 = w1_anal.after.get(bigram[1])
+        for anal1, freq_anal1_given_anal2 in befores_w2_anal.items():
+            w1_anal = get_word(anal1)
+            freq_b2_given_anal1 = w1_anal.after.get(b2)
 
-            if freq3 is not None:
-                # Pe(anal1 | w1 _) * Pe(anal1 | _ anal2) * Pe(w2 | anal2 _),
-                analogies[(anal1, anal2)] = (freq2 / w1_bigram.freq
-                                             * freq1 / w2_anal.freq
-                                             * freq3 / w1_anal.freq)
+            if freq_b2_given_anal1 is not None:
+                # Pe(anal2 | b1 _) * Pe(anal1 | _ anal2) * Pe(b2 | anal1 _),
+                analogies[(anal1, anal2)] = p_anal2_given_w1 * freq_anal1_given_anal2 / anal2_freq * freq_b2_given_anal1 / w1_anal.freq
 
     return analogies
 

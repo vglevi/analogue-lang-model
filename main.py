c6b4410e3953b1de399b7b6ca09e41ce2c33172c
vglevi <zsovalevente@gmail.com>
2026-05-27 21:51:06 +0200

    remove parallel + logging

--- main.py
+++ main.py
@@ -11,15 +11,27 @@ def main():
 
     result = defaultdict(dict)
 
+    print("Finding analogies")
+    nbigrams = len(all_bigrams)
+    i = 0
     for bg in all_bigrams:
         for anal, p in find_analogies(word_dict, bg).items():
             result[anal][bg] = p
+        i += 1
+        print(f"{round(i / nbigrams * 100)}%")
+
+    print("Analogies have been founded")
+    print("Writing them out to out.txt")
 
     with open("out.txt", "w") as f:
         lines = []
+        nresult = len(result)
+        i = 0
         for anal, pb in result.items():
             lines.append(f"{anal}:\n")
             lines.extend(f"\t{bigram}: {p}\n" for bigram, p in pb.items())
+            i += 1
+            print(f"{round(i / nresult * 100)}%")
         f.writelines(lines)
 
 

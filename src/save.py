07a4ac8ba2d0ee881235ead291702c802b7a8ae7
vglevi <zsovalevente@gmail.com>
2026-07-17 18:00:19 +0200

    implementing save_analogies

---
+++ src/save.py
@@ -0,0 +1,15 @@
+import os
+import pickle
+
+from src.analogy import Analogies
+
+
+def save_analogies(bg: tuple[str, str], analogies: Analogies) -> bool:
+    """
+    Saves the 10 most probability increasing anologies in a pickle file named by the bigram.
+    """
+
+    with open(f"bigrams/{bg[0]}_{bg[1]}.pkl", "wb") as f:
+        pickle.dump(sorted(analogies, reverse=True)[:10], f, pickle.HIGHEST_PROTOCOL)
+
+    return True

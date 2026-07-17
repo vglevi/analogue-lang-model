7f17cfdf283c87f76bc62cefeeb576a798bc62ee
vglevi <zsovalevente@gmail.com>
2026-07-17 18:58:11 +0200

    changed main to save analogies in .pkl files

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from collections import defaultdict
+import os
+import shutil
 
 from src.analogy import find_analogies
 from src.analysis import analyze_corpus
 from src.corpus import assign_train_test, process_txt
+from src.save import save_analogies
+
+# from collections import defaultdict
 
 
 def main():
@@ -11,30 +15,39 @@ def main():
     word_dict = analyze_corpus(train)
     all_bigrams = {bg for sen in test for bg in zip(sen, sen[1:])}
 
-    result = defaultdict(dict)
+    # result = defaultdict(dict)
+    try:
+        shutil.rmtree("bigrams")
+    except FileNotFoundError:
+        pass
+
+    print("Creating directory bigrams")
+    os.mkdir("bigrams")
 
     print("Finding analogies")
     nbigrams = len(all_bigrams)
     i = 0
     for bg in all_bigrams:
-        for anal, p in find_analogies(word_dict, bg).items():
-            result[anal][bg] = p
+        save_analogies(bg, find_analogies(word_dict, bg))
+        # for anal, p in find_analogies(word_dict, bg).items():
+        #     result[anal][bg] = p
         i += 1
         print(f"{round(i / nbigrams * 100)}%")
 
-    print("Analogies have been founded")
-    print("Writing them out to out.txt")
-
-    with open("out.txt", "w") as f:
-        lines = []
-        nresult = len(result)
-        i = 0
-        for anal, pb in result.items():
-            lines.append(f"{anal}:\n")
-            lines.extend(f"\t{bigram}: {p}\n" for bigram, p in pb.items())
-            i += 1
-            print(f"{round(i / nresult * 100)}%")
-        f.writelines(lines)
+    # print("Analogies have been founded")
+    # print("Writing them out to out.txt")
+    #
+    # with open("out.txt", "w") as f:
+    #     lines = []
+    #     nresult = len(result)
+    #     i = 0
+    #     for anal, pb in result.items():
+    #         lines.append(f"{anal}:\n")
+    #         lines.extend(f"\t{bigram}: {p}\n" for bigram, p in pb.items())
+    #         i += 1
+    #         print(f"{round(i / nresult * 100)}%")
+    #     f.writelines(lines)
+    #
 
 
 if __name__ == "__main__":
